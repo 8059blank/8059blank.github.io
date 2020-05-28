69c5416287369a33719bf984b06d59eb7b5c8fc4
Ocean <riverkex@gmail.com>
2020-05-28 19:00:49 +0800

    added gallery to gitignore and changed README.md

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Aspera",
-        "excerpt":"Project Demo Intro: I didn’t actually know what I wanted to make, except that it would be a game. Originally I wanted to make a cross between a platformer and a shooter, but I couldn’t think of how to spawn the enemies in a way that would be fun and...","categories": ["individual"],
+        "excerpt":"By Lucas Tan Project Demo Intro: I didn’t actually know what I wanted to make, except that it would be a game. Originally I wanted to make a cross between a platformer and a shooter, but I couldn’t think of how to spawn the enemies in a way that would...","categories": ["individual"],
         "tags": [],
         "url": "http://localhost:4000/individual/aspera/",
         "teaser": null
