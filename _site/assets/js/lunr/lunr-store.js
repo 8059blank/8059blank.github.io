baa35059e33b96247b00ca7be9027f8fa80ad615
Ocean <riverkex@gmail.com>
2020-05-30 21:26:07 +0800

    medacs

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "MedACS",
+        "excerpt":"MedACS (Medicine Automated Control System) is an integrated medication scheduling, provision and reminder system designed to reduce the burden on stay-at-home caregivers and patients by eliminating medicinal nonadherence and protecting users from accidental overdose. Working with a smartphone app, MedACS supersedes existing systems by remotely and automatically tabulating and scheduling...","categories": ["club"],
+        "tags": [],
+        "url": "http://localhost:4000/club/medACS/",
+        "teaser": null
+      },{
         "title": "Aspera",
         "excerpt":"Project Demo Intro: I didn’t actually know what I wanted to make, except that it would be a game. Originally I wanted to make a cross between a platformer and a shooter, but I couldn’t think of how to spawn the enemies in a way that would be fun and...","categories": ["individual"],
         "tags": [],
